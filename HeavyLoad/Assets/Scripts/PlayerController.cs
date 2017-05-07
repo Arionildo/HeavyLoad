2281d851c22d68d1c9f9f25b357f91d9f90d3dfe
ArionildoEscouto <arionildo6@gmail.com>
2017-05-07 16:52:22 -0300

    Add Player Spawn

--- HeavyLoad/Assets/Scripts/PlayerController.cs
+++ HeavyLoad/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,19 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     private float inputMove;
     private float inputTurn;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private bool hasControl;
     public float deadZone = 0.1f;
     public float movespeed = 10f;
     public float turnspeed = 100f;
 
     // Use this for initialization
     private void Start () {
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+        Invoke("EnableControls", 1);
+
         if (GetComponent<Rigidbody>())
             rb = GetComponent<Rigidbody>();
         else
@@ -25,8 +32,10 @@ public class PlayerController : MonoBehaviour {
 	}
 
     private void FixedUpdate() {
-        Move();
-        Turn();
+        if (hasControl) {
+            Move();
+            Turn();
+        }
     }
 
     private void Turn() {
@@ -44,5 +53,23 @@ public class PlayerController : MonoBehaviour {
 
         if (Mathf.Abs(inputMove) > deadZone)
             rb.velocity = transform.forward * inputMove * movespeed;
+    }
+
+    private void OnTriggerEnter(Collider col) {
+        if (col.tag.Equals("Goal"))
+            Respawn();
+    }
+
+    private void Respawn() {
+        hasControl = false;
+        rb.velocity = Vector3.zero;
+        transform.position = originalPosition;
+        transform.rotation = originalRotation;
+
+        Invoke("EnableControls", 1);
+    }
+
+    private void EnableControls() {
+        hasControl = true;
     }
 }
