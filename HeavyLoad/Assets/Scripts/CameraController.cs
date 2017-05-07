b0c031d4d4847eb081aa86bbf5c52f3726bb0826
ArionildoEscouto <arionildo6@gmail.com>
2017-05-07 04:54:06 -0300

    Add Camera Control

---
+++ HeavyLoad/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+
+    // Use this for initialization
+    private void Start () {
+        if (target == null)
+            Debug.LogError("TARGET is missing.");
+	}
+
+    // Update is called once per frame
+    private void Update () {
+        transform.LookAt(target);
+	}
+}
