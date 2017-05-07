9d4c4262ac2fd39fd1e2c615bd602cce4d75d191
ArionildoEscouto <arionildo6@gmail.com>
2017-05-07 04:53:47 -0300

    Add Player Movement

---
+++ HeavyLoad/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float deadZone = 0.1f;
+    public float movespeed = 10f;
+    public float turnspeed = 100f;
+
+    // Use this for initialization
+    private void Start () {
+        if (GetComponent<Rigidbody>())
+            rb = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("RIGIDBODY is missing.");
+    }
+
+    // Update is called once per frame
+    private void Update () {
+
+	}
+
+    private void FixedUpdate() {
+        Move();
+        Turn();
+    }
+
+    private void Turn() {
+        if (Mathf.Abs(rb.velocity.magnitude) > deadZone) {
+            float turn = Input.GetAxis("Horizontal") * turnspeed * Time.deltaTime;
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+            rb.MoveRotation(rb.rotation * turnRotation);
+        }
+    }
+
+    private void Move() {
+        rb.velocity = transform.forward * Input.GetAxis("Vertical") * movespeed;
+    }
+}
