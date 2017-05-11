5ece61261fc5db9b827dd44c8219f53e8e28b401
ArionildoEscouto <arionildo6@gmail.com>
2017-05-11 20:55:19 -0300

    Update GameManager

--- HeavyLoad/Assets/Scripts/GameManager.cs
+++ HeavyLoad/Assets/Scripts/GameManager.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+    private float initialTime;
+    private float currentTime;
+    private Text timerText;
+    private Transform newSpawnPosition;
+    public Transform boxSpawnPoint;
+    public GameObject[] boxes;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        timerText = GameObject.Find("Timer").GetComponent<Text>();
+        initialTime = Time.time;
+
+        if (boxSpawnPoint == null)
+            Debug.LogError("BOXSPAWNPOINT is missing");
+        else
+            newSpawnPosition = boxSpawnPoint;
+
+        GenerateBox();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            SceneManager.LoadScene("Stage01");
-	}
+        CheckEndGame();
+        RunTimer();
+    }
+
+    private void GenerateBox()
+    {
+        int boxIndex = UnityEngine.Random.Range(0, boxes.Length);
+        float xBoxPosition = boxSpawnPoint.position.x;
+        float zBoxPosition = boxSpawnPoint.position.z;
+
+        newSpawnPosition.position = new Vector3(xBoxPosition, boxSpawnPoint.position.y, zBoxPosition);
+        Instantiate(boxes[boxIndex], newSpawnPosition);
+
+        Invoke("GenerateBox", 20);
+    }
+
+    private void CheckEndGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || GameObject.FindGameObjectWithTag("Box") == null)
+            RestartGame();
+    }
+
+    private void RunTimer()
+    {
+        currentTime = Time.time - initialTime;
+        timerText.text = currentTime.ToString("#.##");
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene("Stage01");
+    }
 }
